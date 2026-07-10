7992d1b1eee2541800a4052e78a999cf3d2068f4
mahady13 <mohiuddinmahady@gmail.com>
2026-07-11 00:04:54 +0600

    loaded model onto the main.py file

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ import streamlit as st
 from PIL import Image
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import LabelEncoder
+
+device='cuda' if torch.cuda.is_available() else 'cpu'
+def load_models():
+    model=MyModel()
+    checkpoint=torch.load("best_model.pth",map_location=device)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.to(device)
+    model.eval()
+
+    label_encoder=joblib.load("label_encoder (2).pkl")
+
+    return model,label_encoder
+
+model,label_encoder=load_models()
