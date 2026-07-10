4952b37d091b2c1526958c4fdf48e1f55071b2b7
mahady13 <mohiuddinmahady@gmail.com>
2026-07-11 00:04:12 +0600

    added needed libraries

---
+++ main.py
@@ -0,0 +1,8 @@
+from model_architecture import MyModel
+import torch
+import torchvision.transforms as transforms
+import joblib
+import streamlit as st
+from PIL import Image
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import LabelEncoder
